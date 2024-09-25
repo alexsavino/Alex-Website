35164610c8908753ad3965ac47c468b1e13a0196
Alexandra Savino <avs2167@barnard.edu>
2024-09-25 19:48:12 -0400

    update

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
-    return (
-        <p>TESTING YYOUR MOM</p>
-    )
+  return (
+    <div className="pageContainer">
+      <div className="textContainer">
+        <p className="subtitle">
+          404 Error:
+        </p><p className="title">
+          Thank you for trying to visit my site!
+        </p><p className="subtitle">
+          Unfortunately, the page you're looking for doesn't seem to exist.
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound;
